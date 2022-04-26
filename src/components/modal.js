a4a8a5458e37b854f673face663fc49b6c27b7ae
Alexandr Antonov <santana-plus@mail.ru>
2022-04-26 13:29:38 +0300

    правка ошибок после первого ревью

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,54 +1,32 @@
 import {openPopup, closePopup} from './utils.js';
 
+export const popups = document.querySelectorAll('.popup'); // все попапы
+
 export const popupProfile = document.querySelector('.popup_type_profile'); // попап редактирования профиля
 export const buttonOpenPopupProfile = document.querySelector('.profile__buttom-info'); // кнопка радактирования профиля
-export const buttonClosePopupProfile = popupProfile.querySelector('.popup__button-сlose'); // кнопка закрытия попапа редактирования профиля
 export const formPopupProfile = document.forms.form_profile; // форма попапа редактирования профиля
 const inpuNametPopupProfile = formPopupProfile.elements.name;  // поле ввода имени в форме попап редактирования профиля
 const inputJobPopupProfile = formPopupProfile.elements.character; // поле ввода работы в форме попап редактирования профиля
-
 const userName = document.querySelector('.profile__title');  // элемент "Пользователь"
 const userJob = document.querySelector('.profile__subtitle');  // элемент "Профессия"
 
 export const popupAddCard = document.querySelector('.popup_type_addCard');  // попап добавления новой карточки
 export const buttonOpenPopupAddCard = document.querySelector('.profile__buttom-add');  // кнопка открытия попапа новой карточки
-export const buttonClosePopupAddCard = popupAddCard.querySelector('.popup__button-сlose'); // кнопка закрытия попапа новой карточки
 export const formPopupAddCard = document.forms.form_addCard; // форма попапа добавления новой карточки
 
-
-
-
 export const popupImegeOpen = document.querySelector('.popup_type_openImegeCard');  // oткрытие попапа картинки
-export const buttonClosePopupImage = popupImegeOpen.querySelector('.popup__button-сlose'); // кнопка закрытия попапа новой карточки
-
 const textPopupImage = popupImegeOpen.querySelector('.popup__title-openImegeCard'); // элемент текста попапа картинки
 const urlPopupImage = popupImegeOpen.querySelector('.popup__img'); // элемент url картинки
 
 
 
-// функция закрытия попапа профиля при клике за пределами окана
-export function closePopupOverlayClock (evt, popup) {
-  if (evt.target.classList.contains('popup')) {
-    closePopup(popup);
-  }
-}
 
-// функция закрытия попапа профиля при 'Escape'
-export function setListenerEscape (pop) {
-  document.addEventListener('keydown', evt => {
-    if (evt.key === 'Escape') {
-      closePopup(pop);
-      document.removeEventListener('keydown', evt)
-    }
-  });
-}
 
 // Функция открытие модального окна профиля
 export function openProfile() {
   inpuNametPopupProfile.value = userName.textContent;
   inputJobPopupProfile.value = userJob.textContent;
   openPopup(popupProfile);
-  setListenerEscape(popupProfile);
 }
 
 // Сохраниения профиля  и закрытия модального окна
@@ -60,12 +38,10 @@ export function saveProfile (evt) {
 }
 
 
-
 // Функция открытия попапа новой карточки
 export function openPopupAddCard () {
   formPopupAddCard.reset();
   openPopup(popupAddCard);
-  setListenerEscape(popupAddCard);
 }
 
 
@@ -75,7 +51,6 @@ export function openImegaCard (textImage, urlImage) {
   urlPopupImage.setAttribute('src', urlImage);
   urlPopupImage.setAttribute('alt', textImage);
   textPopupImage.textContent = textImage;
-  setListenerEscape(popupImegeOpen);
 }
 
 
--- src/components/utils.js
+++ src/components/utils.js
@@ -1,5 +1,22 @@
-export const openPopup = function(pop) {pop.classList.add('popup_opened')}; // добавление модификатора открытия popup окна
-export const closePopup = function(pop) {pop.classList.remove('popup_opened')}  // закрытие popup окна
+
+function closeByEscape(evt) {
+  if (evt.key === 'Escape') {
+    const openedPopup = document.querySelector('.popup_opened') // нашли открытый попап
+    closePopup(openedPopup);  //<== закрыли попап с помощью функции `closePopup` ==
+  }
+}
+
+// добавление модификатора открытия popup окна
+export const openPopup = function(pop) {
+  pop.classList.add('popup_opened');
+  document.addEventListener('keydown', closeByEscape);
+};
+
+// закрытие popup окна
+export const closePopup = function(pop) {
+  pop.classList.remove('popup_opened');
+  document.removeEventListener('keydown', closeByEscape);
+}
 
 
 
